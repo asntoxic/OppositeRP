9fbc849d29730c5edea6ab23f511c3b132ac5ce6
Luzz <63018815+asntoxic@users.noreply.github.com>
2021-02-25 14:58:41 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+module.exports = (client) => {
+
+    fs.readdir("./handlers/", (err, files) => {
+        if (err) console.error(err);
+        let jsfiles = files.filter(f => f.split(".").pop() === "js");
+        if (jsfiles.length <= 0) return console.log("There are no events to load...");
+        console.log(`Załadowano ${jsfiles.length} eventów...`);
+        jsfiles.forEach((f, i) => {
+            require(`./handlers/${f}`);
+            console.log(`${i + 1}: ${f} załadowane! ✅`);
+        });
+    });
+}
